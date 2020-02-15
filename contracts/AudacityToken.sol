cbda39c012bc44f30b526d9cb90c76b4ee3b2d77
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-02-16 01:07:24 +0300

    Added basic DAO token

---
+++ contracts/AudacityToken.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.6 <0.7.0;
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+
+/**
+ * @title AudacityToken
+ * @dev The main token for the Audacity DAO
+ * @author gtlewis
+ * @author scorpion9979
+ */
+contract AudacityToken is ERC20Mintable, ERC20Detailed, ERC20Pausable {
+    // TODO: refactor all contracts for 0.6.x to satisfy OpenZeppelin
+    // TODO: remove msg.sender from minter role and add DAO contract address instead
+    constructor() ERC20Detailed('Audacity', 'AUD', 18) public;
+}
