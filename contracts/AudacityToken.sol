d4dc584ce29aa06222f9ae54a17247c43510be51
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-02-26 23:52:08 +0300

    Improved token implementation

--- contracts/AudacityToken.sol
+++ contracts/AudacityToken.sol
@@ -1,5 +1,6 @@
 pragma solidity >=0.6 <0.7.0;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
 /**
@@ -8,8 +9,25 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
  * @author gtlewis
  * @author scorpion9979
  */
-contract AudacityToken is ERC20Detailed, ERC20Pausable {
-    // TODO: remove msg.sender from minter role and add DAO contract address instead
-    constructor() ERC20Detailed('Audacity', 'AUD', 18) public {
+contract AudacityToken is ERC20Detailed, ERC20Capped, ERC20Pausable {
+    // TODO: modify _initialSupply and _maxSupply
+    // TODO: implement a public minting mechanism
+    uint256 private _initialSupply = 10_000e18;
+    uint256 private _maxSupply = 100_000_000_000e18;
+
+    constructor()
+        public
+        ERC20Detailed("Audacity", "AUD", 18)
+        ERC20Capped(_maxSupply)
+    {
+        _mint(_msgSender(), _initialSupply);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        virtual
+        override(ERC20Capped, ERC20Pausable)
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
