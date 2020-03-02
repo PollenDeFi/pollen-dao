aa2bba9dffbd86dcab4d5d51bee351fb0ebda6b8
gtlewis <garethtlewis@gmail.com>
2020-03-02 01:37:37 +0100

    Mintable DAO Token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
-const AudacityToken = artifacts.require("AudacityToken");
+const DAOToken = artifacts.require("DAOToken");
 const AudacityDAO = artifacts.require("AudacityDAO");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(AudacityToken);
-  await deployer.deploy(AudacityDAO, AudacityToken.address);
+  await deployer.deploy(DAOToken);
+  await deployer.deploy(AudacityDAO, DAOToken.address);
 };
---
+++ test/dao-token.js
@@ -0,0 +1,39 @@
+const DAOToken = artifacts.require("DAOToken");
+const { expect } = require('chai');
+const { expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
+
+contract('daoToken', function (accounts) {
+    beforeEach(async function () {
+        this.daoToken = await DAOToken.new();
+    });
+
+    it('should set the owner to be the account that created the token', async function () {
+        const owner = await this.daoToken.owner();
+        expect(owner).to.be.equal(accounts[0]);
+    });
+
+    it('should fail when a non-owner accounts mints', function () {
+        expectRevert(
+            this.daoToken.mint(0, {from: accounts[1]}),
+            'Ownable: caller is not the owner'
+        );
+    });
+
+    it('should create and transfer tokens to the owner account when minting', async function () {
+        let totalSupply;
+        totalSupply = await this.daoToken.totalSupply();
+        expect(totalSupply).to.be.bignumber.equal('0');
+        let balance;
+        balance = await this.daoToken.balanceOf(accounts[0]);
+        expect(balance).to.be.bignumber.equal('0');
+        const receipt = await this.daoToken.mint(1);
+        totalSupply = await this.daoToken.totalSupply();
+        expect(totalSupply).to.be.bignumber.equal('1');
+        balance = await this.daoToken.balanceOf(accounts[0]);
+        expect(balance).to.be.bignumber.equal('1');
+        expectEvent(
+            receipt,
+            'Transfer'
+        );
+    });
+});
