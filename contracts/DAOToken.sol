1eeb6ebf2d759d1b411db71df8e53be56d61d174
Ahmed Ihsan Tawfeeq <ahmed_ihsan_tawfeek@yahoo.com>
2020-05-08 14:11:58 +0000

    Protect against an array of voting attack vectors using snapshots

--- contracts/DAOToken.sol
+++ contracts/DAOToken.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.6 <0.7.0;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/drafts/ERC20Snapshot.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "../node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
 
@@ -10,12 +10,12 @@ import "../node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
  * @author gtlewis
  * @author scorpion9979
  */
-contract DAOToken is ERC20, ERC20Detailed, Ownable {
+contract DAOToken is ERC20Snapshot, ERC20Detailed, Ownable {
     /**
     * @notice Constructor sets the DAO token display values (public)
     */
     constructor() public ERC20Detailed("DAOToken", "DAOT", 18) {}
-    
+
     /**
     * @notice Mint tokens to the owner account (external)
     * @param amount The amount of tokens to mint
@@ -23,5 +23,14 @@ contract DAOToken is ERC20, ERC20Detailed, Ownable {
     function mint(uint256 amount) external onlyOwner
     {
         _mint(owner(), amount);
+    }
+
+    // TODO: make external and use the internal _snapshot() implementation for V3.0.0
+    /**
+    * @notice Creates a new snapshot and returns its snapshot id (public)
+    */
+    function snapshot() public override onlyOwner returns (uint256)
+    {
+        return super.snapshot();
     }
 }
